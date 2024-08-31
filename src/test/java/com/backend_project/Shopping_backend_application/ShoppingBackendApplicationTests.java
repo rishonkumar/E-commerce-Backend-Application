c2a29a9336375a72073c266ed7e29d42c9c27843
Rishon <rishon.kumar.rahi@gmail.com>
2024-08-31 17:15:51 +0530

    Initial project setup

---
+++ src/test/java/com/backend_project/Shopping_backend_application/ShoppingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_project.Shopping_backend_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
