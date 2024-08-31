5e2cb547817ee53076eec31ae636b7199b85c158
Rishon <rishon.kumar.rahi@gmail.com>
2024-08-31 18:15:48 +0530

    Added productService and ProductRepository

---
+++ src/main/java/com/backend_project/Shopping_backend_application/repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.backend_project.Shopping_backend_application.repository;
+
+import com.backend_project.Shopping_backend_application.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    public List<Product> findByCategoryName(String category);
+
+    List<Product> findByBrand(String brand);
+
+    List<Product> findByCategoryNameAndBrand(String category, String brand);
+
+    List<Product> findByName(String name);
+
+    List<Product> findByBrandAndName(String brand, String name);
+
+    Long countByBrandAndName(String brand, String name);
+}
