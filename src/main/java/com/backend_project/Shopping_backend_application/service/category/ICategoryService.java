1960972180780eb6759216c70727d663eddd6b85
Rishon <rishon.kumar.rahi@gmail.com>
2024-09-03 08:58:02 +0530

    Added Category service features/Custom exception

---
+++ src/main/java/com/backend_project/Shopping_backend_application/service/category/ICategoryService.java
@@ -0,0 +1,15 @@
+package com.backend_project.Shopping_backend_application.service.category;
+
+import com.backend_project.Shopping_backend_application.model.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+    Category getCategoryById(Long id);
+    Category getCategoryByName(String name);
+    List<Category> getAllCategories();
+    Category addCategory(Category category);
+    Category updateCategory(Category category, Long id);
+    void deleteCategory(Long id);
+
+}
