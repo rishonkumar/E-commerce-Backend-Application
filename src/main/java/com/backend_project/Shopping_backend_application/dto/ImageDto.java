f36f56aaec2ecf1d9fe369760684a3ae9cdc162c
Rishon <rishon.kumar.rahi@gmail.com>
2024-09-04 16:32:51 +0530

    Added Image service/repository features/Custom exception

---
+++ src/main/java/com/backend_project/Shopping_backend_application/dto/ImageDto.java
@@ -0,0 +1,11 @@
+package com.backend_project.Shopping_backend_application.dto;
+
+import lombok.Data;
+
+@Data
+public class ImageDto {
+    private Long imageId;
+    private String imageName;
+    private String downloadUrl;
+
+}
---
+++ src/main/java/com/backend_project/Shopping_backend_application/service/image/ImageService.java
@@ -0,0 +1,91 @@
+package com.backend_project.Shopping_backend_application.service.image;
+
+import com.backend_project.Shopping_backend_application.dto.ImageDto;
+import com.backend_project.Shopping_backend_application.exceptions.ResourceNotFoundException;
+import com.backend_project.Shopping_backend_application.model.Image;
+import com.backend_project.Shopping_backend_application.model.Product;
+import com.backend_project.Shopping_backend_application.repository.ImageRepository;
+import com.backend_project.Shopping_backend_application.repository.ProductRepository;
+import com.backend_project.Shopping_backend_application.service.product.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.sql.rowset.serial.SerialBlob;
+import javax.sql.rowset.serial.SerialException;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService implements IImageService {
+
+    private final ImageRepository imageRepository;
+    private final IProductService productService;
+
+    @Override
+    public Image getImageById(Long id) {
+        return imageRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Image not found" + id));
+    }
+
+    @Override
+    public void deleteImageById(Long id) {
+        imageRepository.findById(id).ifPresentOrElse(imageRepository::delete, () -> {
+            throw new ResourceNotFoundException("Image not found" + id);
+        });
+
+    }
+
+    @Override
+    public void updateImage(MultipartFile file, Long imageId) {
+        Image image = getImageById(imageId);
+        try {
+            image.setId(imageId);
+            image.setFileName(file.getOriginalFilename());
+            image.setImage(new SerialBlob(file.getBytes()));
+            imageRepository.save(image);
+        } catch (IOException | SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+    }
+
+    @Override
+    public List<ImageDto> saveImages(List<MultipartFile> files, Long productId) {
+        Product product = productService.getProductById(productId);
+        List<ImageDto>savedImageDto = new ArrayList<>();
+        for (MultipartFile file : files) {
+            try {
+                Image image = new Image();
+                image.setFileName(file.getOriginalFilename());
+                image.setFileType(file.getContentType());
+                image.setImage(new SerialBlob(file.getBytes()));
+                image.setProduct(product);
+
+                String buildDownloadUrl = "/api/v1/images/image/download";
+                String downloadUrl = buildDownloadUrl + image.getId();
+                image.setDownloadUrl(downloadUrl);
+
+                Image savedImage = imageRepository.save(image);
+                savedImage.setDownloadUrl(buildDownloadUrl + savedImage.getId());
+                imageRepository.save(image);
+
+                ImageDto imageDto = new ImageDto();
+                imageDto.setImageId(savedImage.getId());
+                imageDto.setImageName(savedImage.getFileName());
+                imageDto.setDownloadUrl(savedImage.getDownloadUrl());
+                savedImageDto.add(imageDto);
+            } catch (SerialException e) {
+                throw new RuntimeException(e);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return savedImageDto;
+    }
+}
