61659eb8b63374b7c21221e53e2ea1fbb7517cea
Rishon <rishon.kumar.rahi@gmail.com>
2024-09-21 17:27:13 +0530

    Created Cart and CartItem entities

---
+++ src/main/java/com/backend_project/Shopping_backend_application/model/CartItem.java
@@ -0,0 +1,43 @@
+package com.backend_project.Shopping_backend_application.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int quantity;
+
+    private BigDecimal unitPrice;
+
+    private BigDecimal totalPrice;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private void setTotalPrice() {
+        this.totalPrice = this.unitPrice.multiply(new BigDecimal(quantity));
+    }
+}
+
+
+
+
